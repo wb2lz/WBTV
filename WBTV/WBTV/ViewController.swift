8a0d020c2506022f580c0d354170d9bc32db5764
wb2lz <wb2lz@wb2lzs-MacBook-Pro.local>
2016-10-01 14:40:40 +0800

    new

---
+++ WBTV/WBTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WBTV
+//
+//  Created by wb2lz on 01/10/2016.
+//  Copyright © 2016 wb2lz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
